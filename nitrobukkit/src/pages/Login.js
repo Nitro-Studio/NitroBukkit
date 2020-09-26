c051e5d108dd0c14b829690dd2a9fd1dcdee9f25
Nitro <nitro0@naver.com>
2020-09-26 20:56:35 +0900

    Login page update

--- nitrobukkit/src/pages/Login.js
+++ nitrobukkit/src/pages/Login.js
@@ -6,6 +6,7 @@ import * as actions from '../reducers/actions'
 import './css/login.css'
 
 import bg from '../assets/img/login_bg.png'
+import login from '../assets/img/login.svg'
 
 class Login extends React.Component {
     render() {
@@ -13,11 +14,44 @@ class Login extends React.Component {
             <>
                 <img src={bg} className='bgimg' />
                 <div className='loginbox'>
+                    <img src={login} className='sideimg' />
                     <div className='sidebox'>
-                        <h2>img</h2>
-                    </div>
-                    <div className='sidebox'>
-                        <h2>Login</h2>
+                        <br />
+                        <br />
+                        <h1>Welcome back!</h1>
+                        <h2>Connect to your server :)</h2>
+                        <br />
+                        <div className='inputbox'>
+                            <p>Server information</p>
+                            <input
+                                className='input ipbox'
+                                type='text'
+                                placeholder='IP Address'
+                                minLength='3'
+                                onChange={(e) => {
+                                    //password = e.target.value
+                                }}
+                            />
+                            <input
+                                className='input portbox'
+                                type='text'
+                                placeholder='Port'
+                                minLength='3'
+                                onChange={(e) => {
+                                    //password = e.target.value
+                                }}
+                            />
+                            <p>Authorization</p>
+                            <input
+                                className='input pwbox'
+                                type='password'
+                                placeholder='Password'
+                                minLength='3'
+                                onChange={(e) => {
+                                    //password = e.target.value
+                                }}
+                            />
+                        </div>
                     </div>
                 </div>
             </>
