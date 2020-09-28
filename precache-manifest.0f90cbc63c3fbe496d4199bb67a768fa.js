81071509e66d24186a4b95531c88bad3294193f2
Nitro <nitro0@naver.com>
2020-09-28 12:38:19 +0900

    Updates

---
+++ precache-manifest.0f90cbc63c3fbe496d4199bb67a768fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c027f17271fa70a68c86cd276fe239",
+    "url": "/NitroBukkit/index.html"
+  },
+  {
+    "revision": "a0bc86b883db3ad8f568",
+    "url": "/NitroBukkit/static/css/main.41a2bdef.chunk.css"
+  },
+  {
+    "revision": "f6bde17748f259cc9b1d",
+    "url": "/NitroBukkit/static/js/2.4e4fdcc0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NitroBukkit/static/js/2.4e4fdcc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0bc86b883db3ad8f568",
+    "url": "/NitroBukkit/static/js/main.016cdcea.chunk.js"
+  },
+  {
+    "revision": "faaad69b3592332270f9",
+    "url": "/NitroBukkit/static/js/runtime-main.9103b657.js"
+  },
+  {
+    "revision": "4fba52c11ccda39667658da15c665556",
+    "url": "/NitroBukkit/static/media/login.4fba52c1.svg"
+  },
+  {
+    "revision": "316c819a5e26ae45cc4a0d4196d41632",
+    "url": "/NitroBukkit/static/media/login_bg.316c819a.png"
+  }
+]);
